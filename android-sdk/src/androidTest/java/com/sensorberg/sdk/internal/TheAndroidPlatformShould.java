34fe9be898eb06cf7b17239acc2c74653628dfba
Sarah Kraynick <sarah.kraynick@sensorberg.com>
2016-04-12 11:20:54 +0200

    changes to expected for beacon id.

--- android-sdk/src/androidTest/java/com/sensorberg/sdk/internal/TheAndroidPlatformShould.java
+++ android-sdk/src/androidTest/java/com/sensorberg/sdk/internal/TheAndroidPlatformShould.java
@@ -80,7 +80,7 @@ public class TheAndroidPlatformShould extends SensorbergApplicationTest {
 
     public void test_get_installation_identifier() {
         AndroidPlatform androidPlatform = new AndroidPlatform(getContext());
-        Assertions.assertThat(androidPlatform.getDeviceInstallationIdentifier()).isEqualTo("58efc13964ed44b4806b0148ae27dc97");
+        Assertions.assertThat(androidPlatform.getDeviceInstallationIdentifier()).isEqualTo("e860745dff144f47bd9ac23b9916a436");
     }
 
 }
