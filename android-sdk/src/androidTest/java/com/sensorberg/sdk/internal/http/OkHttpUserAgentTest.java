e477170dc27acd47ea674e4e5d4cce56140bbf00
Falko Richter <falko@briefhansa.de>
2016-04-25 15:51:24 +0200

    clean advertising identifier

--- android-sdk/src/androidTest/java/com/sensorberg/sdk/internal/http/OkHttpUserAgentTest.java
+++ android-sdk/src/androidTest/java/com/sensorberg/sdk/internal/http/OkHttpUserAgentTest.java
@@ -2,8 +2,9 @@ package com.sensorberg.sdk.internal.http;
 
 import com.android.sensorbergVolley.VolleyError;
 import com.sensorberg.sdk.SensorbergApplicationTest;
-import com.sensorberg.sdk.internal.AndroidPlatform;
 import com.sensorberg.sdk.internal.OkHttpClientTransport;
+import com.sensorberg.sdk.internal.Transport;
+import com.sensorberg.sdk.testUtils.TestPlatform;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.RecordedRequest;
 
@@ -16,13 +17,15 @@ import static org.mockito.Mockito.when;
 public class OkHttpUserAgentTest  extends SensorbergApplicationTest {
 
     private OkHttpClientTransport transport;
-    AndroidPlatform plattform;
+
+    TestPlatform plattform;
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
 
-        plattform = spy(new AndroidPlatform(getContext()));
+        plattform = spy(new TestPlatform());
+        plattform.setContext(getContext());
 
         when(plattform.useSyncClient()).thenReturn(true);
 
@@ -47,5 +50,26 @@ public class OkHttpUserAgentTest  extends SensorbergApplicationTest {
 
         RecordedRequest request = waitForRequests(1).get(0);
         Assertions.assertThat(request.getHeader("User-Agent")).isEqualTo(plattform.getUserAgentString());
+    }
+
+    public void testAdvertisingIdIsSetInVolleyOkHttpHeader() throws Exception {
+
+        transport.setAdvertisingIdentifier("ADVERTISING_ID_TEST");
+
+        server.enqueue(new MockResponse().setBody("{}"));
+        transport.perform(getUrl("/layout").toString(), new com.android.sensorbergVolley.Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+            }
+        }, new com.android.sensorbergVolley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        RecordedRequest request = waitForRequests(1).get(0);
+        Assertions.assertThat(request.getHeader(Transport.ADVERTISING_IDENTIFIER)).isEqualTo("ADVERTISING_ID_TEST");
     }
 }
