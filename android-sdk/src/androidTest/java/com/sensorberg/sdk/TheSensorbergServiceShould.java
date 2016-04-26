8e9e9a7ce45167eea5d6ee668446a1a13fa89c74
Sasa Sekulic <ja@sasasekulic.info>
2016-04-26 14:16:25 +0200

    added tests for setting the advertising id

--- android-sdk/src/androidTest/java/com/sensorberg/sdk/TheSensorbergServiceShould.java
+++ android-sdk/src/androidTest/java/com/sensorberg/sdk/TheSensorbergServiceShould.java
@@ -1,34 +1,31 @@
 package com.sensorberg.sdk;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.test.AndroidTestCase;
-
-import com.sensorberg.sdk.model.server.ResolveAction;
-import com.sensorberg.sdk.presenter.ManifestParser;
 import com.sensorberg.sdk.testUtils.TestPlatform;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.content.Context;
+import android.content.Intent;
+import android.test.AndroidTestCase;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class TheSensorbergServiceShould extends AndroidTestCase {
 
     private static final String NEW_API_TOKEN = "SOME_NEW_API_TOKEN";
     private static final String DEFAULT_API_KEY = "DEFAULT_API_KEY";
 
+    private static final String DEFAULT_AD_ID = "DEFAULT_AD_ID";
+
+    private static final String NEW_AD_ID = "NEW_AD_ID";
+
     SensorbergService tested;
     private Intent CHANGE_API_KEY_MESSAGE;
+
+    private Intent CHANGE_ADVERTISING_ID_MESSAGE;
     private TestPlatform testPlatform;
     private Context spyContext;
 
@@ -42,6 +39,11 @@ public class TheSensorbergServiceShould extends AndroidTestCase {
             CHANGE_API_KEY_MESSAGE = new Intent();
             CHANGE_API_KEY_MESSAGE.putExtra(SensorbergService.MSG_SET_API_TOKEN_TOKEN, NEW_API_TOKEN);
             CHANGE_API_KEY_MESSAGE.putExtra(SensorbergService.EXTRA_GENERIC_TYPE, SensorbergService.MSG_SET_API_TOKEN);
+        }
+        {
+            CHANGE_ADVERTISING_ID_MESSAGE = new Intent();
+            CHANGE_ADVERTISING_ID_MESSAGE.putExtra(SensorbergService.MSG_SET_API_ADVERTISING_IDENTIFIER_ADVERTISING_IDENTIFIER, NEW_AD_ID);
+            CHANGE_ADVERTISING_ID_MESSAGE.putExtra(SensorbergService.EXTRA_GENERIC_TYPE, SensorbergService.MSG_SET_API_ADVERTISING_IDENTIFIER);
         }
 
         tested = new SensorbergService();
@@ -58,5 +60,10 @@ public class TheSensorbergServiceShould extends AndroidTestCase {
     public void should_persist_the_settings_when_getting_a_new_API_token() throws Exception {
         tested.onStartCommand(CHANGE_API_KEY_MESSAGE, -1, -1);
         verify(testPlatform).write(any(ServiceConfiguration.class), any(String.class));
+    }
+
+    public void should_persist_the_settings_when_getting_a_new_advertising_id() throws Exception {
+        tested.onStartCommand(CHANGE_ADVERTISING_ID_MESSAGE, -1, -1);
+        verify(testPlatform.getTransport(), times(1)).setAdvertisingIdentifier(anyString());
     }
 }
--- android-sdk/src/androidTest/java/com/sensorberg/sdk/service/TheServiceConfiguration.java
+++ android-sdk/src/androidTest/java/com/sensorberg/sdk/service/TheServiceConfiguration.java
@@ -1,12 +1,12 @@
 package com.sensorberg.sdk.service;
 
-import android.test.AndroidTestCase;
-
 import com.sensorberg.sdk.ServiceConfiguration;
 import com.sensorberg.sdk.internal.FileHelper;
 import com.sensorberg.sdk.resolver.ResolverConfiguration;
 
 import org.fest.assertions.api.Assertions;
+
+import android.test.AndroidTestCase;
 
 import java.io.File;
 
@@ -15,7 +15,8 @@ public class TheServiceConfiguration extends AndroidTestCase{
     private static final long[] VIBRATION = new long[]{1, 2, 3, 5, 6, 7};
     ServiceConfiguration tested;
     private ResolverConfiguration resolverConf;
-    private String API_TOKEN = "SOEMTHING";
+    private String API_TOKEN = "SOMETHING";
+    private String ADVERTISING_ID = "SOMETHING_ADVERTISING_ID";
 
     @Override
     protected void setUp() throws Exception {
@@ -23,6 +24,7 @@ public class TheServiceConfiguration extends AndroidTestCase{
 
         resolverConf = new ResolverConfiguration();
         resolverConf.setApiToken(API_TOKEN);
+        resolverConf.setAdvertisingIdentifier(ADVERTISING_ID);
 
         tested = new ServiceConfiguration(resolverConf);
     }
@@ -31,10 +33,11 @@ public class TheServiceConfiguration extends AndroidTestCase{
         File file = File.createTempFile("test" + System.currentTimeMillis(),"tmp");
         FileHelper.write(tested, file);
 
-        ServiceConfiguration desrialized = (ServiceConfiguration) FileHelper.getContentsOfFileOrNull(file);
+        ServiceConfiguration deserialized = (ServiceConfiguration) FileHelper.getContentsOfFileOrNull(file);
 
-        Assertions.assertThat(desrialized).isNotNull();
-        Assertions.assertThat(desrialized.resolverConfiguration.apiToken).isEqualTo(API_TOKEN);
-        Assertions.assertThat(desrialized.isComplete()).isTrue();
+        Assertions.assertThat(deserialized).isNotNull();
+        Assertions.assertThat(deserialized.resolverConfiguration.apiToken).isEqualTo(API_TOKEN);
+        Assertions.assertThat(deserialized.resolverConfiguration.getAdvertisingIdentifier()).isEqualTo(ADVERTISING_ID);
+        Assertions.assertThat(deserialized.isComplete()).isTrue();
     }
 }
