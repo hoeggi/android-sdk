11b1d571a8c8d26b24f4a546d66db7c784a44611
Sasa Sekulic <ja@sasasekulic.info>
2016-06-07 14:35:20 +0200

    added explicit access & getters to DefaultSettings and SettingsMgr

--- android-sdk/src/main/java/com/sensorberg/sdk/settings/DefaultSettings.java
+++ android-sdk/src/main/java/com/sensorberg/sdk/settings/DefaultSettings.java
@@ -4,15 +4,15 @@ import com.sensorberg.sdk.Constants;
 
 public class DefaultSettings {
 
-    static final boolean DEFAULT_SHOULD_RESTORE_BEACON_STATE = true;
+    public static final boolean DEFAULT_SHOULD_RESTORE_BEACON_STATE = true;
 
-    static final long DEFAULT_LAYOUT_UPDATE_INTERVAL = Constants.Time.ONE_DAY;
+    public static final long DEFAULT_LAYOUT_UPDATE_INTERVAL = Constants.Time.ONE_DAY;
 
-    static final long DEFAULT_HISTORY_UPLOAD_INTERVAL = 30 * Constants.Time.ONE_MINUTE;
+    public static final long DEFAULT_HISTORY_UPLOAD_INTERVAL = 30 * Constants.Time.ONE_MINUTE;
 
-    static final long DEFAULT_SETTINGS_UPDATE_INTERVAL = Constants.Time.ONE_DAY;
+    public static final long DEFAULT_SETTINGS_UPDATE_INTERVAL = Constants.Time.ONE_DAY;
 
-    static final long DEFAULT_EXIT_TIMEOUT_MILLIS = 9 * Constants.Time.ONE_SECOND;
+    public static final long DEFAULT_EXIT_TIMEOUT_MILLIS = 9 * Constants.Time.ONE_SECOND;
 
     public static final long DEFAULT_FOREGROUND_SCAN_TIME = 10 * Constants.Time.ONE_SECOND;
 
@@ -26,9 +26,9 @@ public class DefaultSettings {
 
     public static final long DEFAULT_MESSAGE_DELAY_WINDOW_LENGTH = Constants.Time.ONE_SECOND * 10;
 
-    static final long DEFAULT_MILLIS_BEETWEEN_RETRIES = 5 * Constants.Time.ONE_SECOND;
+    public static final long DEFAULT_MILLIS_BEETWEEN_RETRIES = 5 * Constants.Time.ONE_SECOND;
 
     public static final long DEFAULT_CACHE_TTL = 30 * Constants.Time.ONE_DAY;
 
-    static final int DEFAULT_MAX_RETRIES = 3;
+    public static final int DEFAULT_MAX_RETRIES = 3;
 }
